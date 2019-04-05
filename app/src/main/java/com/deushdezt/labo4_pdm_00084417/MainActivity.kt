17ab602a10a24607e7f615a9fb8ec73108be028e
Donali <00084417@uca.edu.sv>
2019-04-05 07:22:59 -0600

    Initial commit

---
+++ app/src/main/java/com/deushdezt/labo4_pdm_00084417/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.deushdezt.labo4_pdm_00084417
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
